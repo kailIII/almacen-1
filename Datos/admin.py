068ba6352a0a8a05f673702c3a90ae37c4e4a341
lacio <fabio99_85@hotmail.com>
2014-04-28 09:51:35 -0400

    importacion de modelos y registro de los mismo

--- Datos/admin.py
+++ Datos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from Datos.models import Unidad_Medida
+from Datos.models import Material
+from Datos.models import Area
+from Datos.models import Persona
 
 # Register your models here.
+
+
+
+admin.site.register(Unidad_Medida)
+admin.site.register(Material)
+admin.site.register(Area)
+admin.site(Persona)
