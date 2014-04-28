ecdd4e0ba3ee2edeb34e2384b5a13315ead2d5ca
lacio <fabio99_85@hotmail.com>
2014-04-28 09:44:33 -0400

    Creacion de todas las entidades de la aplacion

--- Datos/models.py
+++ Datos/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class Material(models.Model):
-    descripcion=models.CharField(max_length=100),
-    precio=models.FloatField(null=True, blank=True),
-    
 class Unidad_Medida(models.Model):
-    
+    descripcion=models.CharField(max_length=100)
+    abreviacion=models.CharField(max_length=20, null=True , blank=True)
+
+    def __str__(self):
+        return self.descripcion
+
+class Material(models.Model):
+    descripcion=models.CharField(max_length=100)
+    precio=models.FloatField(null=True, blank=True)
+    unidad=models.ForeignKey(Unidad_Medida, verbose_name='Unidad de Medida')
+    def __str__(self):
+        return self.descripcion
+
+class Area(models.Model):
+    nombre=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Persona(models.Model):
+    nombre=models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
