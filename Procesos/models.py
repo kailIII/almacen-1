da1ac813f1a73bb0666d9a0507f33f8b0577df73
lacio <fabio99_85@hotmail.com>
2014-04-28 11:01:14 -0400

    creacion de la clase Salida_Materia y item_Salida en el modulo de transacciones

--- Procesos/models.py
+++ Procesos/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from Datos.models import Persona
+from Datos.models import Area
+from Datos.models import Material
 # Create your models here.
+
+class Salida_Material(models.Model):
+    fecha=models.DateField(default=date.today())
+    solicitante=models.ForeingKey(Persona, verbose_name='Solicitante')
+    area=models.ForeingKey(Area)
+    autorizante=models.ForeingKey(Persona, verbose_name='Autorizado por')
+    almacenero=models.ForeignKey(Persona,verbose_name='Encargado de almacen')
+    receptor=models.ForeignKey(Persona,verbose_name='Recibido por')
+
+
+    def __str__(self):
+        return 'Salida de material ' + self.id
+
+    class Meta:
+        verbose_name_plural='Salidas de material'
+
+
+class Item_Salida(models.Model):
+    salida_material=models.ForeignKey(Salida_Material)
+    material=models.ForeingKey(Material,verbose_name='Detalle')
+    cantidad=models.FloatField(default=0)
+    observacion=models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Detalle de salida ' + salida_material.id
+
+    
+
